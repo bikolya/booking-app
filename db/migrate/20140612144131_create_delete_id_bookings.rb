735021e58efd4ae70b4c5f0e2dbfa9baf7d6447e
Nikolay Bienko <bikolya@gmail.com>
2014-06-12 19:00:01 +0400

    Fixed another migration problems

--- db/migrate/20140612144131_create_delete_id_bookings.rb
+++ db/migrate/20140612144131_create_delete_id_bookings.rb
@@ -1,5 +1,6 @@
 class CreateDeleteIdBookings < ActiveRecord::Migration
   def change
+    drop_table :bookings
     create_table :bookings do |t|
       t.integer :country_id
       t.integer :flight_id
